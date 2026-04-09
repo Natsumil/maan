fd57cce8a2999dc045da836eb4c1b3178e11d537
Natsumil <kota.yamada@natsumil.net>
2026-04-10 02:23:10 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1313,4 +1313,10 @@ client.on(Events.InteractionCreate, async (interaction: Interaction) => {
   }
 });
 
+import http from 'http';
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Azure Health Check OK');
+}).listen(process.env.PORT || 8080);
+
 client.login(TOKEN);
